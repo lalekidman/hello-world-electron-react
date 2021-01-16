11120a1232cb9022a7d46a97c60fa426584947de
Darryl Fabian <lalekidman@gmail.com>
2021-01-16 22:59:52 +0800

    implement open a helloworld.txt file on the root directory of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { shell } from 'electron';
 import icon from '../assets/icon.svg';
 
-const Hello = () => {
+const MainContainer = () => {
+  const openFile = () => {
+    shell.openPath(`${__dirname}/../helloworld.txt`);
+  };
   return (
     <div>
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1>Open Hello World.txt</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        <button type="button" onClick={openFile}>
+          Open File
+        </button>
       </div>
     </div>
   );
@@ -43,7 +26,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={MainContainer} />
       </Switch>
     </Router>
   );
